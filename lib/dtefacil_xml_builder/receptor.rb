3379f9f8fc027f3bdffea105c7c27e28a96b92f8
vla <vladimir@acid.cl>
2012-08-01 15:19:49 -0400

    se agrega clase receptor

---
+++ lib/dtefacil_xml_builder/receptor.rb
@@ -0,0 +1,14 @@
+module DtefacilXmlBuilder
+	class receptor
+		attr_accessor :rut, :razon_social, :giro, :direccion, :comuna, :ciudad
+
+		def set_receptor receptor
+			:rut 			= receptor[:rut]
+			:razon_social 	= receptor[:razon_social]
+			:giro 			= receptor[:giro]
+			:direccion 		= receptor[:direccion]
+			:comuna 		= receptor[:comuna]
+			:ciudad 		= receptor[:ciudad] 
+		end
+	end
+end
